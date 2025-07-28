d91fffd077452265d5260ad7ce464f2ac08e2393
Vibhav Ahuja <vibhav.ahuja@netsmartz.local>
2025-07-28 17:24:49 +0530

    data preprocess

--- utils/comman_function.py
+++ utils/comman_function.py
@@ -2,6 +2,7 @@ import os
 import yaml
 from src.custom_exception import CustomException
 from src.logger import get_logger
+import pandas as pd 
 
 logger = get_logger(__name__)
 
@@ -19,3 +20,12 @@ def read_yaml(file_path):
     except Exception as e:
         logger.error("Error while reading YAML file")
         raise CustomException("Failed to read YAML file", e)
+    
+
+def load_data(path):
+    try:
+        logger.info("Loading data")
+        return pd.read_csv(path)
+    except Exception as e:
+        logger.error(f"Error while loading data {e}")
+        raise CustomException("Failed to load data", e)
